62e9fa1edb57a3382f86c71f9817457fa9b8951d
kunapot <kunapot@thinknet.co.th>
2017-11-03 15:49:24 +0700

    track different screen

---
+++ src/screens/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Button } from 'react-native'
+import firebase from 'react-native-firebase'
+
+class HomeScreen extends Component {
+  static navigationOptions = {
+    title: 'Welcome',
+  }
+
+  componentWillMount() {
+    firebase.analytics().setCurrentScreen('Home')    
+  }
+
+  navigateToProfileScreen() {
+    firebase.analytics().logEvent('click_profile', {
+      timestamp: new Date().toLocaleString(),
+    })
+
+    const { navigate } = this.props.navigation    
+    navigate('Profile', { name: 'Jane' })
+  }
+
+  render() {
+    
+    return (
+      <Button
+        title="Go to Jane's profile"
+        onPress={() => this.navigateToProfileScreen()}
+      />
+    )
+  }
+}
+
+export default HomeScreen
---
+++ src/screens/Profile.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import { Text, View } from 'react-native'
+import firebase from 'react-native-firebase'
+
+class ProfileScreen extends Component {
+  static navigationOptions = {
+    title: 'Profile',
+  }
+
+  componentWillMount() {
+    firebase.analytics().setCurrentScreen('Profile')    
+  }
+
+  render() {
+    const { name } = this.props.navigation.state.params
+    return (
+      <View style={styles.container}>
+        <Text>{name}</Text>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    alignItems: 'center',
+  },
+}
+
+export default ProfileScreen
